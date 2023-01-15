3af25d0769d7a152030c972c8a909a14d062ec7a
RamanSlinka <romuch1986@gmail.com>
2023-01-15 13:42:22 +0100

    Binding, Event Bindings

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +7,24 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'learn-angular-v2';
+  img = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTNm7TsOwStdKiqs1M2Xp0FOS7TtBp5fbxlKcfi3NKtM0Q-OmT--WStHTvveDVPRlui8A4&usqp=CAU';
+  inputValue = ''
+
+  constructor() {
+    setTimeout(() => {
+      this.img = 'https://angular.io/assets/images/logos/angular/angular.png'
+    }, 5000)
+  }
+
+  inputHandler(event: Event) {
+    this.inputValue = (<HTMLInputElement>event.target).value
+  }
+
+  onClick() {
+    console.log('click')
+  }
+
+  onBlur(str: string) {
+    this.inputValue = str
+  }
 }
