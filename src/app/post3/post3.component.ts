ee78fcb3db4b26ad40ecd3377c6f417c61ddbe58
RamanSlinka <romuch1986@gmail.com>
2023-01-16 21:43:24 +0100

    directives; *ngFor

--- src/app/post3/post3.component.ts
+++ src/app/post3/post3.component.ts
@@ -1,20 +1,26 @@
-import { Component } from '@angular/core';
+import {Component} from '@angular/core';
 
 @Component({
   selector: 'app-post3',
-  template: `
-  <div class="post3">
-    <h2>Post title(test)</h2>
-    <p>This post is created with inline template and inline stiles </p>
-  </div>
-  `,
-  styles: [`
-  .post3{
-    border: 1px gray dashed;
-    margin: 5px;
-    padding: 5px;
-  }`]
+  templateUrl: 'post3.component.html'
 })
 export class Post3Component {
 
+  arr = [1, 1, 2, 3, 5, 8, 13]
+  posts = [
+    {
+      title: 'POST 1', author: 'Greg', comments: [
+        {name: 'Max', text: 'lorem1'},
+        {name: 'Bax', text: 'lorem2'},
+        {name: 'Ka-Ka', text: 'lorem3'},
+      ]
+    },
+    {
+      title: 'POST 2', author: 'Piter', comments: [
+        {name: 'Jax', text: 'lorem1'},
+        {name: 'Ja-Ja', text: 'lorem2'},
+        {name: 'Ra-Jax', text: 'lorem3'},
+      ]
+    },
+  ]
 }
