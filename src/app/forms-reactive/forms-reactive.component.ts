300ae10c7638f9985082750191a1ca0cc31b70b7
RamanSlinka <romuch1986@gmail.com>
2023-01-31 07:43:01 +0100

    Forms and validation. Reactive forms. Create component and add template. Create controls(FormGroup, FormControl). Add validation.

--- src/app/forms-reactive/forms-reactive.component.ts
+++ src/app/forms-reactive/forms-reactive.component.ts
@@ -1,10 +1,11 @@
-import { Component } from '@angular/core';
-
+import {Component} from '@angular/core';
 @Component({
   selector: 'app-forms-reactive',
   templateUrl: './forms-reactive.component.html',
   styleUrls: ['./forms-reactive.component.scss']
 })
 export class FormsReactiveComponent {
+
+
 
 }
